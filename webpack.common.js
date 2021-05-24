c3fd12965d10395bb28e5b0356f0f6ed720808f3
saul-paulus <saulpaulus17@icloud.com>
2021-05-24 16:30:48 +0900

    membuat navbar

---
+++ webpack.common.js
@@ -0,0 +1,74 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Copywebpackplugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/scripts/index'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      /* style and css loader */
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      /* babel loader */
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  /* plugin */
+  plugins: [
+    /* HTML Webpack Plugin */
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/templates/index.html'),
+      filename: 'index.html'
+    }),
+    /* copy-webpack-plugin */
+    new Copywebpackplugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/public'),
+          to: path.resolve(__dirname, 'dist/')
+        }
+      ]
+    }),
+    new CleanWebpackPlugin()
+  ],
+  performance: {
+    hints: false
+  }
+}
