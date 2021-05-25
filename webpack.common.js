2e5670b1b2d50b8f5558b8cdcc5d23cd7b1871e0
saul-paulus <saulpaulus17@icloud.com>
2021-05-25 21:32:58 +0900

    membuat header dan about

--- webpack.common.js
+++ webpack.common.js
@@ -62,7 +62,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, './src/public'),
-          to: path.resolve(__dirname, 'dist/')
+          to: path.resolve(__dirname, 'dist')
         }
       ]
     }),
